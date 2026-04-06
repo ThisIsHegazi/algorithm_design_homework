3e0ad86e5227a4e55310969bbd57dbad9612458a
Ahmed Hegazy <ahmedhegazy26105@gmail.com>
2026-04-06 10:13:44 +0200

    sort logic put inside a function

--- insertion_sort/insersion.py
+++ insertion_sort/insersion.py
@@ -4,17 +4,22 @@ for _ in range(n):
     x.append(int(input(f"x[{_}] ")))
 print(x)
 
-i = 1
-while i < n:  # n
-    val = x[i]
-    j = i - 1
-    while j > -1:  # n
-        if val < x[j]:
-            x[j + 1] = x[j]
-        else:
-            break
-        j -= 1
-    x[j + 1] = val
-    i += 1
+
+def insertion_sort(x: list) -> list:
+    i = 1
+    while i < n:  # n
+        val = x[i]
+        j = i - 1
+        while j > -1:  # n
+            if val < x[j]:
+                x[j + 1] = x[j]
+            else:
+                break
+            j -= 1
+        x[j + 1] = val
+        i += 1
+    return x
+
+
 # n * n +(some non important 1s) => O(n ** 2)
-print(x)
+print(insertion_sort(x))
