9b9341e8c7f9ea8607548d98c51fe73c0c553370
Ahmed Hegazy <ahmedhegazy26105@gmail.com>
2026-04-15 12:50:40 +0200

    Added huffman coding implementation

--- greedy_algorithm/huffman_coding/huffman.py
+++ greedy_algorithm/huffman_coding/huffman.py
@@ -1,46 +1,51 @@
 from collections import Counter
 import heapq
 
+INTERNAL_CHAR = chr(0)
+
 
 class HeapNode:
-    def __init__(self, data, freq) -> None:
-        self.data = data
+    def __init__(self, char, freq) -> None:
+        self.data = char
         self.freq = freq
-        self.right: HeapNode | None = None
-        self.left: HeapNode | None = None
+        self.right = None
+        self.left = None
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: HeapNode):
         return self.freq < other.freq
 
 
 class Huffman:
-    def __init__(self, message: str) -> None:
-        self.frequencies = Counter(message)
-        self.codes = {}
+    def __init__(self, msg) -> None:
+        self.codes = dict()
+        self.msg = msg
+        # get freqs
+        self.freqHash = Counter(msg)
+        self.minHeap = []
+        # create the minHeap(priorty queue)
+        for k, v in self.freqHash.items():
+            node = HeapNode(k, v)
+            heapq.heappush(self.minHeap, node)
+        # create the heap(binary tree for huffman codes)
+        while len(self.minHeap) > 1:
+            left_node = heapq.heappop(self.minHeap)
+            right_node = heapq.heappop(self.minHeap)
+            top = HeapNode(INTERNAL_CHAR, left_node.freq + right_node.freq)
+            top.left = left_node
+            top.right = right_node
+            heapq.heappush(self.minHeap, top)
+        # generate codes of the message
+        self.generate_codes(self.minHeap[0], "")
 
-    def generate_codes(self, node: HeapNode, st):
+    def generate_codes(self, node: HeapNode, code):
         if node is None:
             return
-        if node.data != internal_data:
-            self.codes[node.data] = st
-        self.generate_codes(node.left, f"{st}0")
-        self.generate_codes(node.right, f"{st}1")
+        if node.data != INTERNAL_CHAR:
+            self.codes[node.data] = code
+        self.generate_codes(node.left, code + "0")
+        self.generate_codes(node.right, code + "1")
 
 
-internal_data = None
-pq: list[HeapNode] = []
-msg = Huffman("internet")
-for k, v in msg.frequencies.items():
-    top = HeapNode(k, v)
-    heapq.heappush(pq, top)
-
-while len(pq) > 1:
-    left_node = heapq.heappop(pq)
-    right_node = heapq.heappop(pq)
-    top = HeapNode(internal_data, left_node.freq + right_node.freq)
-    if left_node and right_node:
-        top.left = left_node
-        top.right = right_node
-    heapq.heappush(pq, top)
-msg.generate_codes(pq[0], "")
-print(msg.codes)
+msg = "internet"
+huff = Huffman(msg)
+print(huff.codes)
